725b3d862e438cb592d098d0797af4f3fe8bcfe4
Mariya Mandryk <marysiaminnie@gmail.com>
2022-11-20 15:16:52 -0600

    testing Docker environment variables and aws cli sample command

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM ubuntu:latest
+
+RUN apt-get update && apt-get install curl unzip -y
+
+WORKDIR /s3_migration
+
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
+    && unzip awscliv2.zip \
+    && ./aws/install \
+    && rm awscliv2.zip
+
+COPY ./scripts /s3_migration
+
+RUN chmod +x ./main.sh
+
+ENTRYPOINT ["sh", "./main.sh"]
--- run.sh
+++ run.sh
@@ -1,32 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
-# aws s3 cp \
-# s3://mariya-20221119-assignment1/0486-GRAF-BIG-KNOB.pdf \
-# s3://mariya-20221119-assignment1-copy/0486-GRAF-BIG-KNOB.pdf \
-# --sse
-   # aws s3 ls \
-# s3://mariya-20221119-assignment1/0486-GRAF-BIG-KNOB.pdf \
-# --output json
+exitFatal() {
+    echo 'command failed. Exiting...'
+    exit 1
+}
 
-# aws s3api head-object \
-# --bucket mariya-20221119-assignment1 \
-# --key 0486-GRAF-BIG-KNOB.pdf --query 
-
-
-limit_MB=5
-limit_BYTE=$((limit_MB*1024*1024))
-
-echo $limit_BYTE
-
-aws s3api list-objects --bucket mariya-20221119-assignment1 --output json --query '[Contents[].{Key: Key, Size: Size}]' > output_all.json
-aws s3api list-objects --bucket mariya-20221119-assignment1 --output json --query "[Contents[?Size>$limit_BYTE].{Key: Key, Size: Size}]" > output.json
- 
- 
- 
-
- 
- 
- 
-  
- 
- 
+docker image rm s3_migration:0.0.1 --force || exitFatal
+docker image build --platform linux/x86_64 -t s3_migration:0.0.1 . || exitFatal
+docker run --platform linux/x86_64 --env-file aws_credentials s3_migration:0.0.1 || exitFatal
